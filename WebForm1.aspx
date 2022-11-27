c84bd975b717aeef991d872e15b4cadea240b382
Xyrna <mammadov.mahammad2002@gmail.com>
2022-11-27 20:31:52 +0400

    Добавьте файлы проекта.

---
+++ WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="view.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="StyleSheet1.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <div class="center-y">
+        <div class="center-x">
+            <form id="form1" runat="server">
+                <div class="form-controls">
+                <asp:Label ID="Label" runat="server" Text="CategoryName: "></asp:Label>
+                <asp:TextBox ID="TxtInput" runat="server"></asp:TextBox>
+                <asp:Button CssClass="button" AutoPostBack="true" ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+                </div>
+                <asp:GridView  ID="GridView1" runat="server" OnSelectedIndexChanged="GridView_SelectedIndexChanged">
+                </asp:GridView>
+                <asp:Label ID="LblNotFound" runat="server" Text=""></asp:Label>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace view
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+
+        private SqlConnection conn = new SqlConnection("Server=DESKTOP-2QKN505\\SQLEXPRESS; Database=Northwind; Integrated Security=true;");
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            DataTable dt = new DataTable();
+            string query = "select * from ProCat";
+            if (TxtInput.Text != "")
+            {
+                query += " where CategoryName = '" + TxtInput.Text + "';";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                LblNotFound.Text = "";
+                if(dt.Rows.Count == 0)
+                {
+                    GridView1.DataSource = null;
+                    GridView1.DataBind();
+                    LblNotFound.Text = "Result is not found";
+                    conn.Close();
+                    return;
+                }
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+                conn.Close();
+            }
+            else
+            {
+                GridView1.DataSource = null;
+                GridView1.DataBind();
+                LblNotFound.Text = "Result is not found";
+            }
+        }
+    }
+}
